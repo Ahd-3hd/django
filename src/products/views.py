64e85a847ba7d558ed3e6a2771998dbf04d7ac87
ahd <ahdhani0@gmail.com>
2021-03-06 02:36:31 +0300

    handle 404/DoesNotExist

--- src/products/views.py
+++ src/products/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from django.http import Http404
 
 
 def dynamic_lookup_view(request,id):
-    obj = Product.objects.get(id=id)
-    print(obj)
+    # obj = Product.objects.get(id=id)
+    # obj = get_object_or_404(Product,id=id)
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
     context = {
         "object":obj
     }
