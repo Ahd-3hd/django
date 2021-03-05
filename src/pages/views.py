427ea96a002f60aee1836902172845be979da512
ahd <ahdhani0@gmail.com>
2021-03-05 23:19:22 +0300

    try request argument

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args,**kwargs):
+def home_view(request,*args,**kwargs):
+    print(request)
+    print(request.user)
     return HttpResponse("<h1>Hello World!!</h1>")
 
 
-def contact_view(*args,**kwargs):
+def contact_view(request,*args,**kwargs):
+    print(request)
     return HttpResponse("<h1>Contact Page</h1>")
