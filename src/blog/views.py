da407676a859157fa3e3b4cdcb5c87989e76a301
ahd <ahdhani0@gmail.com>
2021-03-06 04:59:11 +0300

    create blog app

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Article
+from .forms import ArticleForm
 # Create your views here.
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleForm
+    queryset = Article.objects.all()
+
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleForm
+    queryset = Article.objects.all()
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article,id=id_)
+
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
 
 class ArticleListView(ListView):
     template_name = "articles/article_list.html"
